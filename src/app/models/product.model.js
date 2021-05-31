ed728326ffa1311773989e83b1d7a4cc4c087aa4
Canhntc <18520513@gm.uit.edu.vn>
2021-05-31 08:19:28 +0700

    nothing

--- src/app/models/product.model.js
+++ src/app/models/product.model.js
@@ -8,38 +8,38 @@ const { Schema } = mongoose;
 
 //for slug generater
 mongoose.plugin(slug, {
-  separator: "-",
-  lang: "en",
-  truncate: 120
+    separator: "-",
+    lang: "en",
+    truncate: 120
 });
 
 
 const property = new Schema({
-  kind: { type: String, default: 'static', enum: ['static', 'customized'], required: true, },
-  key: { type: String, default: '', required: true, },
-  value: { type: Array, default: [], required: true, },
-  fluctuatedAmount: { type: Number, default: 0 }, // can be negative number
+    kind: { type: String, default: 'static', enum: ['static', 'customized'], required: true, },
+    key: { type: String, default: '', required: true, },
+    value: { type: Array, default: [], required: true, },
+    fluctuatedAmount: { type: Number, default: 0 }, // can be negative number
 });
 
 const product = new Schema({
-  name: { type: String, default: '', trim: true, required: true, },
-  images: [{ type: String, default: '', }],
-  properties: [property],
-  categories: [{ type: mongoose.Types.ObjectId, ref: 'categorie', }],
-  supplier: { type: mongoose.Types.ObjectId, ref: 'supplier', },
-  description: { type: String, default: '', trim: true, required: true },
-  orginalprice: { type: Number, required: true, min: 0 },
-  slug: { type: String, slug: "name", unique: true },
+    name: { type: String, default: '', trim: true, required: true, },
+    images: [{ type: String, default: '', }],
+    properties: [property],
+    categories: [{ type: mongoose.Types.ObjectId, ref: 'categorie', }],
+    supplier: { type: mongoose.Types.ObjectId, ref: 'supplier', },
+    description: { type: String, default: '', trim: true, required: true },
+    orginalprice: { type: Number, required: true, min: 0 },
+    slug: { type: String, slug: "name", unique: true },
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 
 //for delete
 product.plugin(mongoose_delete, {
-  deleteAt: true,
-  deleteBy: true,
-  overrideMethods: true,
+    deleteAt: true,
+    deleteBy: true,
+    overrideMethods: true,
 })
 
 module.exports = mongoose.model("product", product); //collection: products
