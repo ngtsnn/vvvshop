d433bd734897be94e6871d17e9e736bb6996dc2d
Canhntc <18520513@gm.uit.edu.vn>
2021-06-07 23:53:16 +0700

    FE admin chart

--- src/app/routes/admins.routes.js
+++ src/app/routes/admins.routes.js
@@ -5,5 +5,12 @@ router.get("/", (req, res, next) => {
   res.render("sites/admin/index");
 });
 
+router.get("/edit", (req, res, next) => {
+  res.render("sites/admin/edit");
+});
+
+router.get("/add", (req, res, next) => {
+  res.render("sites/admin/add");
+});
 
 module.exports = router;
